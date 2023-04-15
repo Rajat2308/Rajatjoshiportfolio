53b930508cf3b2a143abe0db366354075ffce53a
Rajat2308 <111409270+Rajat2308@users.noreply.github.com>
2023-04-16 02:56:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,166 @@
+function myFunction() {
+    var element = document.getElementById("col1");
+    element.classList.add("shadow-lg");
+    element.classList.add("fst-italic");
+  }
+  function myFunction1() {
+    var element = document.getElementById("col2");
+    element.classList.add("shadow-lg");
+    element.classList.add("fst-italic");
+  }
+  function myFunction2() {
+    var element = document.getElementById("col3");
+    element.classList.add("shadow-lg");
+    element.classList.add("fst-italic");
+  }
+  function myFunction3() {
+    var element = document.getElementById("col4");
+    element.classList.add("shadow-lg");
+    element.classList.add("fst-italic");
+  }
+  function profunc1() {
+    var element = document.getElementById("project1");
+    element.classList.add("shadow-lg");
+    element.classList.add("fst-italic");
+  }
+  function profunc2() {
+    var element = document.getElementById("project2");
+    element.classList.add("shadow-lg");
+    element.classList.add("fst-italic");
+  }
+  
+  function profunc3() {
+    var element = document.getElementById("project3");
+    element.classList.add("shadow-lg");
+    element.classList.add("fst-italic");
+  }
+  //ksjdfbshdbshjg
+  function myfunc() {
+    var element = document.getElementById("col1");
+    element.classList.remove("shadow-lg");
+    element.classList.remove("fst-italic");
+  }
+  function myfunc1() {
+    var element = document.getElementById("col2");
+    element.classList.remove("shadow-lg");
+    element.classList.remove("fst-italic");
+  }
+  function myfunc2() {
+    var element = document.getElementById("col3");
+    element.classList.remove("shadow-lg");
+    element.classList.remove("fst-italic");
+  }
+  function myfunc3() {
+    var element = document.getElementById("col4");
+    element.classList.remove("shadow-lg");
+    element.classList.remove("fst-italic");
+  }
+  function profunction1() {
+    var element = document.getElementById("project1");
+    element.classList.remove("shadow-lg");
+    element.classList.remove("fst-italic");
+  }
+  function profunction2() {
+    var element = document.getElementById("project2");
+    element.classList.remove("shadow-lg");
+    element.classList.remove("fst-italic");
+  }
+  
+  function profunction3() {
+    var element = document.getElementById("project3");
+    element.classList.remove("shadow-lg");
+    element.classList.remove("fst-italic");
+  }
+  //certificateAdd
+ function certificateadd1() {
+    var element = document.getElementById("c1");
+    // element.classList.add("shadow-lg");
+    element.classList.add("fw-bolder");
+  }
+  function certificateadd2() {
+    var element = document.getElementById("c2");
+    // element.classList.add("shadow-lg");
+    element.classList.add("fw-bolder");
+  }
+  function certificateadd3() {
+    var element = document.getElementById("c3");
+    // element.classList.add("shadow-lg");
+    element.classList.add("fw-bolder");
+  }
+  function certificateadd4() {
+    var element = document.getElementById("c4");
+    // element.classList.add("shadow-lg");
+    element.classList.add("fw-bolder");
+  }
+  function certificateadd5() {
+    var element = document.getElementById("c5");
+    // element.classList.add("shadow-lg");
+    element.classList.add("fw-bolder");
+  }
+  function certificateadd6() {
+    var element = document.getElementById("c6");
+    // element.classList.add("shadow-lg");
+    element.classList.add("fw-bolder");
+  } 
+  //certificteRemove
+  function certificateremove1() {
+    var element = document.getElementById("c1");
+    // element.classList.remove("shadow-lg");
+    element.classList.remove("fw-bolder");
+  }
+  function certificateremove2() {
+    var element = document.getElementById("c2");
+    // element.classList.remove("shadow-lg");
+    element.classList.remove("fw-bolder");
+  }
+  function certificateremove3() {
+    var element = document.getElementById("c3");
+    // element.classList.remove("shadow-lg");
+    element.classList.remove("fw-bolder");
+  }
+  function certificateremove4() {
+    var element = document.getElementById("c4");
+    // element.classList.remove("shadow-lg");
+    element.classList.remove("fw-bolder");
+  }
+  function certificateremove5() {
+    var element = document.getElementById("c5");
+    // element.classList.remove("shadow-lg");
+    element.classList.remove("fw-bolder");
+  }
+  function certificateremove6() {
+    var element = document.getElementById("c6");
+    // element.classList.remove("shadow-lg");
+    element.classList.remove("fw-bolder");
+  }
+//social
+function gitAdd() {
+    var element = document.getElementById("git");
+    element.classList.add("shadow-lg");
+    // element.classList.add("fst-italic");
+  }
+  function linkedAdd() {
+    var element = document.getElementById("linked");
+    element.classList.add("shadow-lg");
+    // element.classList.add("fst-italic");
+  }
+  function leetAdd() {
+    var element = document.getElementById("leet");
+    element.classList.add("shadow-lg");
+    // element.classList.add("fst-italic");
+  }
+  function gitRemove() {
+    var element = document.getElementById("git");
+    element.classList.remove("shadow-lg");
+    // element.classList.remove("fst-italic");
+  }
+  function linkedRemove() {
+    var element = document.getElementById("linked");
+    element.classList.remove("shadow-lg");
+    // element.classList.remove("fst-italic");
+  }
+  function leetRemove() {
+    var element = document.getElementById("leet");
+    element.classList.remove("shadow-lg");
+    // element.classList.remove("fst-italic");
+  }
